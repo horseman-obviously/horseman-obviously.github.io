a080995abfd74d9f5c3d5d9d269fdaf459f0377a
horseman-obviously <43479414+horseman-obviously@users.noreply.github.com>
2018-10-08 20:55:25 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,3 +18,18 @@ function closeNav() {
     document.getElementById("container").style.marginLeft = "0";
     document.getElementById("hambutton").style.visibility = "initial";
 }
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
